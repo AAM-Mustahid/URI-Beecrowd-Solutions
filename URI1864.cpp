ca9e4a917230e122c661755092c2d41095b345ba
Mustahid <mustahid34@gmail.com>
2022-06-21 01:17:17 +0600

    Our Days are never coming back

---
+++ URI1864.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    string a = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    cin >> n;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i];
+    }
+    cout << endl;
+
+    return 0;
+}
