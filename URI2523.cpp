1201ae13917f28b4048597613bfb5240b8053b0f
Mustahid <mustahid34@gmail.com>
2022-06-23 00:36:35 +0600

    will's Message

---
+++ URI2523.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a[26];
+    int bulb[26];
+    int n, i;
+    while (cin >> a)
+    {
+        cin >> n;
+       while(n--)
+        {
+            cin >> i;
+            cout << a[i - 1];
+        }
+        /* for (i = 1; i <= n; i++)
+         {
+             cout << a[bulb[i] - 1];
+         }
+         */
+         cout << endl;
+        
+    }
+
+    return 0;
+}
