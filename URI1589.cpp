e338cdd2af5bbc7fc224cbe447bce78a69e613f6
Mustahid <mustahid34@gmail.com>
2022-06-23 01:02:39 +0600

    Bob Conduit

---
+++ URI1589.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t, result, r1, r2;
+    cin >> t;
+    while (t--)
+    {
+        cin >> r1;
+        cin >> r2;
+        result = r1+r2;
+        cout << result << endl;
+    }
+
+    return 0;
+}
