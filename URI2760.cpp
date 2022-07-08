9f72a2edf40912aad79ec73f8feaaed5e6feca8a
Mustahid <mustahid34@gmail.com>
2022-07-08 16:49:55 +0600

    String Input and Output

---
+++ URI2760.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a, b, c;
+    getline(cin, a);
+    getline(cin, b);
+    getline(cin, c);
+    cout << a << b << c << endl;
+    cout << b << c << a << endl;
+    cout << c << a << b << endl;
+    int i = 0;
+    while (a[i] != '\0' && i < 10)
+    {
+        cout << a[i];
+        i++;
+    }
+    i = 0;
+    while (b[i] != '\0' && i < 10)
+    {
+        cout << b[i];
+        i++;
+    }
+    i = 0;
+    while (c[i] != '\0' && i < 10)
+    {
+        cout << c[i];
+        i++;
+    }
+    cout << endl;
+
+    return 0;
+}
