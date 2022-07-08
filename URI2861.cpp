d8afb18c54d246663e24581034c617687fd9f91d
Mustahid <mustahid34@gmail.com>
2022-07-08 10:06:35 +0600

    The Output

---
+++ URI2861.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t, i;
+    string ques;
+    cin >> t;
+    for (i = 0; i <= t; i++)
+    {
+
+        getline(cin, ques);
+
+        if (i != 0)
+            cout << "gzuz" << endl;
+    }
+
+    return 0;
+}
