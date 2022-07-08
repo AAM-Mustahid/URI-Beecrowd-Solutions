2c11fd7b29ab2d5501e7679c9f214b99c35f90b7
Mustahid <mustahid34@gmail.com>
2022-07-08 16:08:05 +0600

    Back to School Physics

---
+++ URI1197.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v, t, res;
+
+    while (cin >> v >> t)
+    {
+        res = v * (2 * t);
+        cout << res << endl;
+    }
+
+    return 0;
+}
