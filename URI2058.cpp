c2d9c979497e077572da3a061510af892007fc66
Mustahid <mustahid34@gmail.com>
2022-08-16 01:25:14 +0600

    Trinagles and Regular Polygons

---
+++ URI2058.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, result;
+    cin >> n;
+    result = n - 2;
+    cout << result << endl;
+
+    return 0;
+}
